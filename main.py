f6de09c1f620c97b53020eecbe0d60d9d20e4189
masoudk <masoudkav@gmail.com>
2024-02-10 10:43:09 +0330

    Added Shiraz to cities,Added a new function to get recomendation based on city's weather

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ def get_weather(city):
 
     return WEATHER_DATA.get(city.title(),"no data available for this city")
 
+def get_recomendation(condition):
+    if condition =="rainy":
+        return "dont forget umbrella"
+    elif condition =="sunny":
+        return "wear some sunscreen!"
+    else:
+        return "have a great day"
+
 
 def main():
     city = input("enter the name of the city to get its current weather: ")
