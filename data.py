525989b47db1fe32fec66c1f8318d8a31f6d7e63
masoudk <masoudkav@gmail.com>
2024-02-10 11:08:57 +0330

    Added paris to cities

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ WEATHER_DATA = {
     "London": {"temperature": "15 C", "condition": "cloudy"},
     "New york": {"temperature": "20 C", "condition": "rainy"},
     "Tokyo": {"temperature": "20 C", "condition": "rainy"},
-    "Shiraz": {"temperature": "20 C", "condition": "rainy"}
+    "Shiraz": {"temperature": "20 C", "condition": "rainy"},
+    "Paris": {"temperature": "20 C", "condition": "rainy"}
 
 }
